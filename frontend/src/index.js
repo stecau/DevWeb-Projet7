65e655417ce6d0f98632d6d9f5656a75c9217b09
Stephane CAU <stephane-jean-noel.cau@orange.fr>
2022-09-28 14:00:05 +0200

    test de l'intallation avec le cours OC React

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,22 @@
+/*-----------------------------------------------------------------------------*/
+/* Rendu/Initialisation de notre application React 'app' pour notre FrontEnd : */
+/*-----------------------------------------------------------------------------*/
+
+/* Importation des modules de React */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+/* Importation de style 'css' de base (body & code) */
+import './styles/index.css';
+
+/* Importation de notre notre App FrontEnd */
+import App from './components/App';
+
+/* Déclaration de l'emplacement du contenu 'ReactDOM' (=DOM temporaire) qui sera généré par React pour la page html (utilisation de l'id 'root' de la <div>) */
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Rendu du DOM temporaire 'root' en l'emplacement demandé
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
